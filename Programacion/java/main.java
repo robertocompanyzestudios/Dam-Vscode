9bd1db8e8c7573a4407de55b5e69f90905b682c3
robertocompanyzestudios <robertocompanyzestudios@gmail.com>
2024-11-18 14:03:15 +0100

    commit diario

--- Programacion/java/main.java
+++ Programacion/java/main.java
@@ -3,5 +3,6 @@ package Programacion.java;
 public class main {
     public static void main(String[] args) {
         System.out.println("Odio java");
+        System.out.println("Cada vez odio mas java");
     }
 }
